e54de52a7bd413238c6e713b0b4f340715d0ddfc
Ryan <tobycammockelliott@gmail.com>
2021-02-02 14:04:55 +1300

    Create Mad Lib.py

---
+++ Mad Lib.py
@@ -0,0 +1,28 @@
+#imports the "time" module of python.
+import time
+#Section under reads user input.- then saves it to the variables.
+val1=input("Enter a noun: ")
+time.sleep(0.1)
+val2=input("Enter another noun: ")
+time.sleep(0.1)
+val3=input("enter another noun: ")
+time.sleep(0.1)
+val4=input("enter a proper noun: ")
+time.sleep(0.1)
+Personname=input("enter a persons name: ")
+time.sleep(0.1)
+Bestfreind=input("enter the name of your best freind: ")
+print("Compiled")
+#begins printing variables into the sentences. Enjoy!!!
+time.sleep(1)
+print("Seriously the cafeteria in our school is INSANE!")
+time.sleep(1)
+print("The other day, My freind " +Bestfreind+ "And I walked in there, and the people serving the food look like they're from "+val3)
+time.sleep(1)
+print(" I looked over and see one of them serving fried" +val1+"!")
+time.sleep(1)
+print("There was a student, i think his name was" +Personname+ ", He was poking a fork at what looked like roast"+val2+"!")
+time.sleep(1)
+print("I walked up to get my food and one of the servers shouted" +val3+ ("at me!"))
+time.sleep(1.5)
+print("I decided to just pour tomato sauce on my baked" +val4+ "that made it more bearable")
