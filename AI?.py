0189057a01df954b788eeb2d7ef35d792bc1372a
Ryan <tobycammockelliott@gmail.com>
2021-02-05 10:00:05 +1300

    Update AI?.py

---
+++ AI?.py
@@ -0,0 +1,13 @@
+import time
+Threshold=12
+while(True):
+    age=int(input("What is your age human?: "))
+    if (age>Threshold):
+        print("You count as human ")
+    elif(age<Threshold):
+        print("You are younger than year 9, you are excused ")
+    else:
+        print("You are a 12 year old Year 9, die year 9 ")
+
+
+
