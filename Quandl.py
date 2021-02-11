acd59350d425823b89c7150e6323fc0b3c5fc178
Ryan <tobycammockelliott@gmail.com>
2021-02-11 18:15:34 +1300

    Update Quandl.py

--- Quandl.py
+++ Quandl.py
@@ -1,8 +1,17 @@
 import quandl
+ElapsedTime=0
 import matplotlib.pyplot as plt
 import time
 while(True):
+    ElapsedTime=ElapsedTime+1
+    print("Time elapsed={}".format(ElapsedTime))
+
+    mydata = quandl.get([ "WIKI/NZD.4"])
+    print(mydata)
+
+
     time.sleep(0.1)
-    mydata = quandl.get("RATEINF/INFLATION_NZL")
+
+
 
 
