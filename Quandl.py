896c7c62699fa1753c8ca76826bda91b9b5445d7
Ryan <tobycammockelliott@gmail.com>
2021-02-15 09:32:49 +1300

    Update Quandl.py

--- Quandl.py
+++ Quandl.py
@@ -1,17 +1,22 @@
 import quandl
-ElapsedTime=0
+import pandas
+
+ElapsedTime = 0
 import matplotlib.pyplot as plt
 import time
-while(True):
-    ElapsedTime=ElapsedTime+1
-    print("Time elapsed={}".format(ElapsedTime))
 
-    mydata = quandl.get([ "WIKI/NZD.4"])
-    print(mydata)
+while (True):
+    ElapsedTime = ElapsedTime + 1
+    # print("Time elapsed={}".format(ElapsedTime))
+
+    YearData = quandl.get("RATEINF/CPI_NZL", authtoken="1Yy_wztTrMMBr6AoCATM", collapse='weekly', column_index='1')
+    YearData = str(YearData)
+    # YearData2=YearData.strip('Value')
+    # NewYear=[int(i) for i in YearData.split() if i.isalpha()]
+    YearNew=slice(1, 5)
+    print(YearData[YearNew])
 
 
-    time.sleep(0.1)
+    # print(YearData2.split(','))
 
-
-
-
+    time.sleep(100)
