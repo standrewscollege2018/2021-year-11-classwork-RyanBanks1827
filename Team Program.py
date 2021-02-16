36cba0e1848d050a7fa1a8c1f479eb392254b1af
Ryan <tobycammockelliott@gmail.com>
2021-02-17 08:45:21 +1300

    Update Team Program.py

--- Team Program.py
+++ Team Program.py
@@ -1,6 +1,10 @@
+import time
+versionnumber = 0.2
+BTD = 0.3
 # Registers the List variable of the program.
 Teamnames = []
-
+TeamPeople = []
+teammembers=[]
 
 def command_add(teamtoregister):
     print("Registered the team :" + str(teamtoregister))
@@ -8,16 +12,35 @@ def command_add(teamtoregister):
     list.append(Teamnames, registerteam)
 
 
-def command_remove(Teamtoremove):
-    print("Haha not implemented yet!")
+def command_remove(teamtoremove):
+    print("deleting team "+teamtoremove)
+    teamtoremove.clear()
 
 
-# Importing modules
-import time
+def command_register(person):
+    AddID = input("Which team do you wish to assign this person to?")
+    if AddID in Teamnames:
+        # AddID is used to describe what team you wish to add the person to
+        # Person is just the imported variable used in the function
+        # Remember that, Toby!
 
-BTD = 0.1
-Currentcommand = ("NULL")
-versionnumber = 0.1
+        print("Assigned person to the team: " + AddID)
+        time.sleep(BTD)
+        teampeople = [person, AddID]
+        teammembers = [[Teamnames], [teampeople]]
+
+
+    else:
+        print("Oops, It appears that the """ + AddID +
+              """team doesn't exist! Please use the add command to make this team before assigning people!""")
+    time.sleep(BTD)
+def command_list_members(Teamname):
+    print("The list of people in the " +Teamname+" Team is as follows")
+    time.sleep(BTD)
+    print(teammembers)
+
+Currentcommand = "NULL"
+
 # Main loop
 while (True):
     print("Welcome to the team manager!" + "Version " + str(versionnumber))
@@ -27,11 +50,19 @@ while (True):
     Currentcommand = input("->")
 
     if Currentcommand == "help" or Currentcommand == "Help":
-        print("Use the command Add-Capital or Non capital to create a new team")
+        print("Use the command Add to create a new team")
         time.sleep(BTD)
-        print("Use the command Remove to delete a stored team- Capital or non capital")
+        print("Use the command Remove to delete a stored team.")
         time.sleep(BTD)
         print("Use the command Register to begin registering a person to a currently existing team")
+        time.sleep(BTD)
+        print("Use the command List to list all current teams registered.")
+        time.sleep(BTD)
+
+        print("Use the memberlist command to see which people are in which team! ")
+        print("""SIDE NOTE: All commands can be entered with a capital 
+                                   first letter or lower case first letter
+                                    which is there purely for convenience.""")
         time.sleep(BTD)
 
 
@@ -41,22 +72,37 @@ while (True):
         teamadd = input("-->")
         command_add(teamadd)
 
+
     elif Currentcommand == "Remove" or Currentcommand == "remove":
         # Simple remove command --coming soon--
 
         print("Which team do you wish to remove?")
 
-    elif Currentcommand == "Read" or Currentcommand == "read":
+    elif Currentcommand == "List" or Currentcommand == "list":
         print("The current teams are :")
         time.sleep(BTD)
-        print(Teamnames)
-        if Teamnames== "[]":
-            print("Oops, looks like you havn't added a team yet!, Make sure you do that before attempting to list!")
+
+        if len(Teamnames) == 0:
+            print("Oops, looks like you havn't  any teams yet!, Make sure you do that before attempting to list!")
+            time.sleep(BTD)
 
         else:
-            print("""Use Read or read followed by the name of the team, 
-                          in order to read the list of team members!""")
+            print(Teamnames)
             time.sleep(BTD)
+            time.sleep(BTD)
+            print("""Use List or list followed by the name of the team, 
+                          in order to read the list of team members!""")
+
+            time.sleep(BTD)
+
+    elif Currentcommand == "Register" or Currentcommand == "register":
+        entity = input("What is the name of the person to register?")
+        time.sleep(BTD)
+        command_register(person=entity)
+
+    elif Currentcommand == "Memberlist" or Currentcommand =="memberlist":
+        whichteamtolist =input("Which team do you wish to retrieve all members from?")
+        command_list_members(whichteamtolist)
 
 
 
@@ -68,3 +114,6 @@ while (True):
 
     else:
         print("use the command help to look at a list of commands ")
+    time.sleep(BTD)
+    time.sleep(BTD)
+    time.sleep(BTD)
