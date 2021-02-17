e062f7eca3df1684b2f172cd3c63b8fea81a58d0
Ryan <tobycammockelliott@gmail.com>
2021-02-18 10:54:11 +1300

    Update Team program TEST.py

--- Team program TEST.py
+++ Team program TEST.py
@@ -1,28 +1,25 @@
-Teams=[]
-Teamnames=[]
+Teams = []
+Teamnames = []
+Exit=False
 while True:
     # command selection from user
     print("Welcome to the Team management program. Please enter a command")
-    currentcommand=input("-->")
+    currentcommand = input("-->")
     # If setup is run
-    if currentcommand==("Setup"):
+    if currentcommand == "Setup":
         print("What do you wish the team to be called?")
-        currentsetupteam=(input("-->"))
-
-        print("Who do you you wish to add to the team? Type exit when you're done")
-        currentpersontoadd = (input("-->"))
-
-        if currentcommand != ("exit"):
-        list.append(Teamnames, currentsetupteam)
-        list.append(Teams[[currentpersontoadd]])
-
-
+        currentsetupteam = (input("-->"))
+# This section is the list filling functions which appends certain items to a list.
+        while not Exit:
+            print("Who do you you wish to add to the team? Type exit when you're done")
+            currentpersontoadd = (input("-->"))
+            list.append(Teams, [currentpersontoadd])
+            list.append(Teamnames, [currentsetupteam])
             print(Teamnames)
             print(Teams)
+            if currentpersontoadd=="exit":
+                Exit=True
 
-
-
-
-
-
-
+        print(Teamnames)
+        print(Teams)
+        print("NiceS")
