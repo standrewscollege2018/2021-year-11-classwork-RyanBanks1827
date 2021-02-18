7bce87fe69539ffc55b86b4bee3be2f34d799f7f
Ryan <tobycammockelliott@gmail.com>
2021-02-18 17:14:57 +1300

    Create Raffle.py

---
+++ Raffle.py
@@ -0,0 +1,31 @@
+
+
+
+while True:
+    print("Welcome to the raffle program!")
+    print("Please enter your name to be in to win!")
+    currententered = input("-->")
+    Working= isinstance(currententered,int)
+    Working= isinstance(currententered, float)
+    print(Working)
+
+    if currententered == "Dev_result":
+        print("Test")
+    elif Working==True:
+        print("Haha, die sucker")
+
+    elif Working ==False:
+        print("Yup")
+
+str=name
+for letter in name:
+    try:
+        test=int(letter)
+        error=true
+        break
+
+    except:
+        pass
+if error ==True
+    if check(input()):
+        print("error")
