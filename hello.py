01a6940a69cb121591a88c169acd48f97d20b9d4
Ryan <tobycammockelliott@gmail.com>
2021-02-22 11:21:05 +1300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,12 +1,12 @@
-#This code should do something awesome- It will Begin Turtle, And begin drawing a complex polygon#
+# This code should do something awesome- It will Begin Turtle, And begin drawing a complex polygon#
 FdVal=2
 AngVal=0.1
 
 import turtle
 turtle.hideturtle()
-turtle.speed(10)
+turtle.speed(5)
 while(True):
     turtle.forward(FdVal)
     turtle.right(AngVal)
-    FdVal=FdVal+0.8
-    AngVal=AngVal+0.4
+    FdVal=FdVal+0.6
+    AngVal=AngVal+1
