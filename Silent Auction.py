c1cd69aa33f6c54c151481ba34d54935e0687bea
Ryan <tobycammockelliott@gmail.com>
2021-02-23 14:33:04 +1300

    Create Silent Auction.py

---
+++ Silent Auction.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import ttk
+root = Tk()
+ttk.Button(root, text="Hello World").grid()
+
+
+while True:
+    if ttk.Button("Hello World"):
+        print("hi")
+root.mainloop()
