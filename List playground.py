b603b0ef0d3726cdb337ca07e099b36aba77bc51
Ryan <tobycammockelliott@gmail.com>
2021-03-01 08:57:12 +1300

    Update List playground.py

--- List playground.py
+++ List playground.py
@@ -1,17 +1,23 @@
 # For lists we use square brackets
 # Items must be split by comma
-Items = ["Item1", "Item2"]
+# Items = ["Item1", "Item2"]
 
-print(Items[0])
+# print(Items[0])
 
 # To add something to the end of a list, use append()
-Items.append("Dr Evil")
+# Items.append("Dr Evil")
 
 # To add something directly into a list, use the Items.insert() or list.insert()
 
-Items.insert(1, "Dr good")
+# Items.insert(1, "Dr good")
 
 # Lists are mutable, they can be written to and deleted from
 # You can override a value of an item in the list, like this!
-Items[1]= "Not any doctor here!"
-print(Items)
+# Items[1]= "Not any doctor here!"
+# print(Items)
+
+
+List = [["Tobias", 15]]
+Listoprint=List
+Listoprint.append(["Gavith", 15])
+print(List)
