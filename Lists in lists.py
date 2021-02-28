91ebb7da30a5011bd2c1fc13409981338b236049
Ryan <tobycammockelliott@gmail.com>
2021-03-01 08:57:14 +1300

    Create Lists in lists.py

---
+++ Lists in lists.py
@@ -0,0 +1,15 @@
+people = [["Dr Evil", 45], ["Gru", 34], ["Emporer Palpatine", 500]]
+
+# Alternative way
+
+names = ["Dr Evil", "Gru", "Emperor"]
+ages = [45, 34, 500]
+
+# print a sub-list
+print(people[1])
+# Print items in sub-list
+print(people[1][1])
+# Cool! Isn't it!
+Retrievename=(str(people[1][0]))
+Retrieveage=(str(people[1][1]))
+print("Hi, this is "+Retrievename+", He is age "+Retrieveage)
