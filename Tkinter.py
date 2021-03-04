dc111b59d1cd05405c4177d6dc23d31af4a88f99
Ryan <tobycammockelliott@gmail.com>
2021-03-05 09:56:51 +1300

    Update Tkinter.py

--- Tkinter.py
+++ Tkinter.py
@@ -1,6 +1,11 @@
+def helloworld():
+    print("Hello World")
+
+
 from tkinter import *
 # Creating the root obj
 root=Tk()
+
 # Set title of the window
 root.title("First GUI")
 # Set if window is resizable
@@ -9,5 +14,13 @@ root.resizable(width=FALSE, height=FALSE)
 root.geometry("500x500")
 # makes sure the darn window doesn't close
 Label1= Label(root, text="Hi!", fg="#FF0000", bg="#FF3172")
+hellobtn=Button(root, text="Say hello", command=helloworld)
+stufflist=["Hi", "Bye", "Coolio"]
+selectedstuff=StringVar()
+selectedstuff.set(stufflist[0])
+selectedstuff=OptionMenu(mainWindow, selectedstuff, stufflist)
+selectedstuff.pack()
+hellobtn.pack()
+
 Label1.pack(side=LEFT)
 root.mainloop()
