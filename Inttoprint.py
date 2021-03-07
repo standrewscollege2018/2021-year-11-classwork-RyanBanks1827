2c35cc7a029b69fb36530abb4e3a2e7e2437aef3
Ryan <tobycammockelliott@gmail.com>
2021-03-08 09:46:28 +1300

    Create Inttoprint.py

---
+++ Inttoprint.py
@@ -0,0 +1,13 @@
+# Get selection
+intfunny=input("Enter an integer from 1-5")
+# A crude way to print the number word corresponding with the input
+if intfunny=="1":
+    print("One")
+elif intfunny=="2":
+    print("Two")
+elif intfunny=="3":
+    print("Three")
+elif intfunny=="4":
+    print("Four")
+elif intfunny=="5":
+    print("Five")
